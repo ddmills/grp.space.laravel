c693609a7fbcd1a9f7f0ce48dbdcf0e65ebd79ed
Dalton Mills <dmills@sourceallies.com>
2016-02-24 21:26:17 -0600

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('subheader')
+    <h2 class='app-subheader-title'>404</h2>
+    <p class='app-subheader-tagline'>All those who wander are not lost.</p>
+@endsection
+
+@section('content')
